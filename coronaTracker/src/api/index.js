97e85c78c0e50946c66c144480822ab6fe42900d
deyRupak <rupakdey9999@gmail.com>
2020-05-09 21:03:05 +0530

    init charts

--- coronaTracker/src/api/index.js
+++ coronaTracker/src/api/index.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 const url = ' https://covid19.mathdro.id/api'
 
 export const fetchData = async () => {
+    
     try{
         const {data: {confirmed, recovered, deaths, lastUpdate}} = await axios.get(url)
         const modifiedData = {
@@ -13,7 +14,26 @@ export const fetchData = async () => {
         }
         return modifiedData
     }
+
     catch(error){
 
     }
+
 }
+
+export const fetchDailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`)
+        
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate
+        }));
+
+        return modifiedData
+    }
+    catch(error){
+
+    }
+} 
